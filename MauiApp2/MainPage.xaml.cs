7288cbcc5bf2c8d88311b035f64bef90966b4245
Victor Opanasyuk <darkelf141@gmail.com>
2024-12-07 01:47:38 +0200

    Change loading screen

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace MauiApp2
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace MauiApp2
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            BlazorWebViewHandler.BlazorWebViewMapper.AppendToMapping("MyBlazorCustomization", (handler, view) => {
+#if IOS
+        handler.PlatformView.Opaque = false;
+        handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+#elif WINDOWS
+                handler.PlatformView.Opacity = 0;
+                handler.PlatformView.DefaultBackgroundColor = new Windows.UI.Color() { A = 0, R = 0, G = 0, B = 0 };
+#endif
+            });
         }
     }
 }
